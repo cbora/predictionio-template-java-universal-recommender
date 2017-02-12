f681d6888027587550d25bc53c4b8b6454e77af2
Chris Fifty <cjf92@cornell.edu>
2017-02-12 17:04:27 -0500

    translated PreparedData from scala to java

--- src/main/java/org/template/recommendation/PreparedData.java
+++ src/main/java/org/template/recommendation/PreparedData.java
@@ -1,15 +1,27 @@
 package org.template.recommendation;
 
+import org.apache.predictionio.data.storage.PropertyMap;
+import org.apache.spark.api.java.JavaPairRDD;
+
 import java.io.Serializable;
+import java.util.Map;
 
 public class PreparedData implements Serializable {
-    private final TrainingData trainingData;
+    private final Map<String, JavaPairRDD<String,String>> actions;
+    private final JavaPairRDD<String,PropertyMap> fieldsRDD;
 
-    public PreparedData(TrainingData trainingData) {
-        this.trainingData = trainingData;
+    public PreparedData(Map<String,JavaPairRDD<String,String>> actions, JavaPairRDD<String,PropertyMap> fieldsRDD) {
+        this.actions = actions;
+        this.fieldsRDD = fieldsRDD;
     }
 
-    public TrainingData getTrainingData() {
-        return trainingData;
+    public Map<String,JavaPairRDD<String,String>> getActions() {
+        return actions;
+    }
+
+    public JavaPairRDD<String,PropertyMap> getFieldsRDD() {
+        return fieldsRDD;
     }
 }
+
+
