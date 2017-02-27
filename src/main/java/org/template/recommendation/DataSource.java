6cb947425c8b13411d76a71f3ea2ccc89a84089b
cbora <borachristian@gmail.com>
2017-02-27 04:18:54 -0500

    First working version of DataSource

--- src/main/java/org/template/recommendation/DataSource.java
+++ src/main/java/org/template/recommendation/DataSource.java
@@ -1,41 +1,62 @@
-// package org.template.recommendation
-import com.google.common.collect.ImmutableMap;
-import io.prediction.data.store.java.PJavaEventStore;
-import scala.collection.JavaConversion;
-import scala.collection.Seq;
-import io.prediction.core.EventWindow;
+package org.template.recommendation;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.HashSet;
+import org.apache.predictionio.core.SelfCleaningDataSource$class;
+import org.apache.predictionio.data.storage.*;
+import org.apache.predictionio.core.SelfCleaningDataSource;
+import org.apache.predictionio.data.store.java.OptionHelper;
+import org.apache.predictionio.controller.EmptyParams;
+import org.apache.predictionio.core.EventWindow;
+import org.apache.predictionio.controller.PDataSource;
+import org.apache.predictionio.data.store.java.PJavaEventStore;
+
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.rdd.RDD;
+import org.apache.spark.SparkContext;
+import org.apache.spark.api.java.JavaRDD;
+
+import org.joda.time.DateTime;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+
+import scala.Option;
+import scala.Tuple2;
+import scala.collection.immutable.Set;
+import scala.collection.Iterable;
+
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
 
 
-public class DataSource extends PDataSource<TrainingData, EmptyParams, Query, Set<String>>{    
+public class DataSource extends PDataSource<TrainingData, EmptyParams, Query, Set<String>> implements SelfCleaningDataSource {
 
-    public final DataSourceParams dsp; // Data source param object
+    private DataSourceParams dsp;                                                               // Data source param object
+    private transient static final Logger logger = LoggerFactory.getLogger(DataSource.class);
+    transient PEvents pEventsDb = Storage.getPEvents();
+    transient private LEvents lEventsDb = Storage.getLEvents(false);
 
+    /* Constructor
+     *
+     * @param datasourceparams: parameters of the datasource 
+     */
     public DataSource(DataSourceParams dsp) {
-
+        super();
         this.dsp = dsp;
-        // Draw info
+
         /***
-        drawInfo("Init DataSource", Seq(
-                                        ("===================", "==================="),
-                                        ("App name", dsp.getAppName()),
-                                        ("Event window", dsp.getEventWindow()),
-                                        ("Event names", dsp.getEventNames())
-                                        ))
-        **/
-        
+         // Draw info
+         drawInfo("Init DataSource", Seq(
+         ("===================", "==================="),
+         ("App name", dsp.getAppName()),
+         ("Event window", dsp.getEventWindow()),
+         ("Event names", dsp.getEventNames())
+         ))
+         **/
     }
 
     /* Getter
      * @return appName
      */
-    public String getAppName() {
+    @Override
+    public String appName() {
         return dsp.getAppName();
     }
 
@@ -47,61 +68,172 @@ public class DataSource extends PDataSource<TrainingData, EmptyParams, Query, Se
     }
 
     /* Getter
+     * @return datasourceparams
+     */
+    public DataSourceParams getDataSourceParams() {
+        return dsp;
+    }
+
+    /* Getter
+     * @param SparkContext object
      * @retrun new TrainingData object
      */
     public TrainingData readTraining(SparkContext sc) {
 
-        ArrayList<String> EventNames = dsp.getEventNames(); // get event names
+        List<String> eventNames = (dsp.getEventNames()); // get event names
 
         // find events associated with the particular app name and eventNames
         JavaRDD<Event> eventsRDD = PJavaEventStore.find(
-                dsp.getAppName(),                       // app name
-                OptionHelper.<~>none(),                 // channel name
-                OptionHelper.<DateTime>none(),          // start time
-                OptionHelper.<DateTime>none(),          // end time
-                OptionHelper.some("user"),              // entity type
-                OptionHelper.<~>none(),                 // entity id
-                dsp.getEventNames(),                    // event names
-                OptionHelper.some("item"),              // target entity type
-                OptionHelper.<Option<String>>none(),    // target entity id
-                sc                                      // spark context
-        ).repartition(sc.defaultParallelism);
+                dsp.getAppName(),                                               // app name
+                OptionHelper.<String>none(),                                    // channel name
+                OptionHelper.<DateTime>none(),                                  // start time
+                OptionHelper.<DateTime>none(),                                  // end time
+                OptionHelper.some("user"),                                // entity type
+                OptionHelper.<String>none(),                                    // entity id
+                OptionHelper.some(eventNames),                                  // event names
+                OptionHelper.some(OptionHelper.some("item")),             // target entity type
+                OptionHelper.<Option<String>>none(),                            // target entity id
+                sc                                                              // spark context
+        ).repartition(sc.defaultParallelism());
 
 
-
-        // Now separate events by event name
-        JavaPairRDD<String, JavaPairRDD<String, String>> eventRDD =
-            eventNames.mapToPair(
-                                 new PairFunction<String, String, Tuple2<String, JavaPairRDD<String, String>>() {
-                                     public Tuple2<String, JavaPairRDD> call(String e) {
-                                         singleEventRDD = eventsRDD.filter(
-                                                                           new Function<JavaRDD<Event>, Boolean>() {
-                                                                               public Boolean call(JavaRDD<Event> event){
-                                                                                   // missing some checks
-                                                                                   return e.isEqual(event.event());
-                                                                               }
-                                                                           };
-                                                                           )
-                                         return new Tuple2(e, singleEventRDD);
-                                     }
-                                 };
-                                 );
+        JavaSparkContext jsc = new JavaSparkContext();
+        JavaRDD<String> eNames = jsc.parallelize(eventNames);
+        JavaRDD<Tuple2<String, JavaRDD<Tuple2<String, String>>>> eventRDD =
+                eNames.map(
+                        eventName -> {
+                            JavaRDD<Tuple2<String, String>> singleEventRDD =
+                                    eventsRDD.filter(
+                                            event -> {
+                                                return !event.eventId().isEmpty() && !event.targetEntityId().get().isEmpty() && eventName.equals(event.event());
+                                            }).map(
+                                            event -> {
+                                                return new Tuple2<String, String>(event.entityId(),
+                                                        event.targetEntityId().get());
+                                            });
+                            return new Tuple2<String, JavaRDD<Tuple2<String, String>>>(eventName, singleEventRDD);
+                        }).filter(pair -> !pair._2().isEmpty());
 
         // convert the eventRDD to a list
-        List<String, JavaPairRDD<String, String>> eventRDDs = eventRDD.collect();           
+        List<Tuple2<String, JavaRDD<Tuple2<String, String>>>> eventRDDs = eventRDD.collect();
 
-        JavaPairRDD<String, PropertyMap> fieldsRDD = PJavaEventStore.aggregateProperties(
-                dsp.getAppName(),                           // app name
-                OptionHelper.some("item"),                  // entity type
-                OptionHelper.<~>none(),                     // channel name
-                OptionHelper.<DateTime>none(),              // start time
-                OptionHelper.<DateTime>none(),              // end time
-                OptionHelper.<List<String>>none(),          // required entities
-                sc                                          // spark context
-        ).repartition(sc.defaultParallelism);
+        JavaRDD<Tuple2<String, PropertyMap>> fieldsRDD = PJavaEventStore.aggregateProperties(
+                dsp.getAppName(),                                  // app name
+                "item",                                  // entity type
+                OptionHelper.<String>none(),                       // channel name
+                OptionHelper.<DateTime>none(),                     // start time
+                OptionHelper.<DateTime>none(),                     // end time
+                OptionHelper.<List<String>>none(),                 // required entities
+                sc                                                 // spark context
+        ).repartition(sc.defaultParallelism());
+
+        //JavaPairRDD<String, PropertyMap> fieldsRDD = JavaPairRDD.fromJavaRDD(tmpFieldsRDD);
+        logger.info("Received event: " + eventRDD.map(e -> e._1()));
+        logger.info("Number of events: " + eventRDDs.size());
 
         return new TrainingData(eventRDDs, fieldsRDD);
     }
 
+    public LEvents org$apache$predictionio$core$SelfCleaningDataSource$$lEventsDb() {
+        return lEventsDb;
+    }
+
+    public PEvents org$apache$predictionio$core$SelfCleaningDataSource$$pEventsDb() {
+        return pEventsDb;
+    }
+
+    public org.apache.predictionio.core.SelfCleaningDataSource.DateTimeOrdering$ DateTimeOrdering() {
+        //TO-DO
+        return null;
+    }
+
+    @Override
+    public grizzled.slf4j.Logger logger() {
+        return this.logger();
+    }
+
+    private boolean isSetEvent(Event e) {
+        return e.event() == "$set" || e.event() == "$unset";
+    }
+
+
+    @Override
+    public Option<EventWindow> eventWindow() {
+        // TO-DO
+        //return new OptionHelper.some(dsp.getEventWindow());
+        return null;
+    }
+
+    @Override
+    public RDD<Event> getCleanedPEvents(RDD<Event> pEvents) {
+        return SelfCleaningDataSource$class.getCleanedPEvents(this, pEvents);
+    }
+
+    public Iterable<Event> getCleanedLEvents(Iterable<Event> lEvents) {
+        return SelfCleaningDataSource$class.getCleanedLEvents(this, lEvents);
+    }
+
+    @Override
+    public RDD<Event> compressPProperties(SparkContext sc, RDD<Event> rdd) {
+        return SelfCleaningDataSource$class.compressPProperties(this, sc, rdd);
+    }
+
+    @Override
+    public Iterable<Event> compressLProperties(Iterable<Event> events) {
+        return SelfCleaningDataSource$class.compressLProperties(this, events);
+    }
+
+    @Override
+    public RDD<Event> removePDuplicates(SparkContext sc, RDD<Event> rdd) {
+        return SelfCleaningDataSource$class.removePDuplicates(this, sc, rdd);
+    }
+
+    @Override
+    public Iterable<Event> removeLDuplicates(Iterable<Event> ls) {
+        return SelfCleaningDataSource$class.removeLDuplicates(this, ls);
+    }
+
+    @Override
+    public Event recreateEvent(Event x, Option<String> eventId, DateTime creationTime) {
+        return SelfCleaningDataSource$class.recreateEvent(this, x, eventId, creationTime);
+    }
+
+    @Override
+    public void cleanPersistedPEvents(SparkContext sc) {
+        SelfCleaningDataSource$class.cleanPersistedPEvents(this, sc);
+    }
+
+    @Override
+    public void wipePEvents(RDD<Event> newEvents, RDD<String> eventsToRemove, SparkContext sc) {
+        SelfCleaningDataSource$class.wipePEvents(this, newEvents, eventsToRemove, sc);
+    }
+
+    @Override
+    public void removeEvents(Set<String> eventsToRemove, int appId) {
+        SelfCleaningDataSource$class.removeEvents(this, eventsToRemove, appId);
+    }
+
+    @Override
+    public void removePEvents(RDD<String> eventsToRemove, int appId, SparkContext sc) {
+        SelfCleaningDataSource$class.removePEvents(this, eventsToRemove, appId, sc);
+    }
+
+    @Override
+    public void wipe(Set<Event> newEvents, Set<String> eventsToRemove) {
+        SelfCleaningDataSource$class.wipe(this, newEvents, eventsToRemove);
+    }
+
+    @Override
+    public RDD<Event> cleanPEvents(SparkContext sc) {
+        return SelfCleaningDataSource$class.cleanPEvents(this, sc);
+    }
+
+    public void cleanPersistedLEvents() {
+        SelfCleaningDataSource$class.cleanPersistedLEvents(this);
+    }
+
+    @Override
+    public scala.collection.Iterable<Event> cleanLEvents() {
+        return SelfCleaningDataSource$class.cleanLEvents(this);
+    }
 }
-    
