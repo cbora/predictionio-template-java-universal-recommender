44e0695c3ac1cf1323fa49fdc0e5a97c28cfcd31
Chris Fifty <cjf92@cornell.edu>
2017-03-03 00:10:13 -0500

    switched rdd to JavaPairRDD in preparator files

--- src/main/java/org/template/recommendation/TrainingData.java
+++ src/main/java/org/template/recommendation/TrainingData.java
@@ -4,26 +4,25 @@ import java.util.List;
 import org.apache.predictionio.data.storage.PropertyMap;
 import org.apache.predictionio.controller.SanityCheck;
 import org.apache.spark.api.java.JavaPairRDD;
-import org.apache.spark.rdd.RDD;
 import scala.Tuple2;
 
 import java.io.Serializable;
 
 
 public class TrainingData implements Serializable, SanityCheck {
-    private final List<Tuple2<String, RDD<Tuple2<String,String>>>> actions;
-    private final RDD<Tuple2<String,PropertyMap>> fieldsRDD;
+    private final List<Tuple2<String, JavaPairRDD<String,String>>> actions;
+    private final JavaPairRDD<String,PropertyMap> fieldsRDD;
 
 
-    public TrainingData(List<Tuple2<String, RDD<Tuple2<String,String>>>> actions, RDD<Tuple2<String,PropertyMap>> fieldsRDD) {
+    public TrainingData(List<Tuple2<String, JavaPairRDD<String,String>>> actions, JavaPairRDD<String,PropertyMap> fieldsRDD) {
         this.actions = actions;
         this.fieldsRDD = fieldsRDD;
     }
 
-    public List<Tuple2<String, RDD<Tuple2<String,String>>>> getActions() {
+    public List<Tuple2<String, JavaPairRDD<String,String>>> getActions() {
         return actions;
     }
-    public RDD<Tuple2<String,PropertyMap>> getFieldsRDD() {
+    public JavaPairRDD<String,PropertyMap> getFieldsRDD() {
         return fieldsRDD;
     }
 
