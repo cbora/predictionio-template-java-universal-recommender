18938f130481d922b6c109710fead60cce5cda24
Yifan <yz542@cornell.edu>
2017-03-08 22:42:09 -0500

    add newRowCardinality

--- src/main/java/org/template/recommendation/indexeddataset/IndexedDatasetJava.java
+++ src/main/java/org/template/recommendation/indexeddataset/IndexedDatasetJava.java
@@ -5,8 +5,7 @@ import org.apache.mahout.sparkbindings.indexeddataset.IndexedDatasetSpark;
 import org.apache.mahout.math.drm.CheckpointedDrm;
 import org.apache.spark.SparkContext;
 import org.apache.mahout.sparkbindings.SparkDistributedContext;
-import org.apache.spark.api.java.JavaRDD;
-import scala.Tuple2;
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.predictionio.data.store.java.OptionHelper;
 
 import java.util.Optional;
@@ -45,7 +44,7 @@ public class IndexedDatasetJava {
         ids.dfsWrite(dest, ids.dfsWrite$default$2(), sc);
     }
 
-    public static IndexedDatasetJava apply(JavaRDD<Tuple2<String, String>> rdd,
+    public static IndexedDatasetJava apply(JavaPairRDD<String,String> rdd,
                                            Optional<BiDictionaryJava> existingRowIDs,
                                            SparkContext sc){
         if (!existingRowIDs.isPresent()){
@@ -55,5 +54,11 @@ public class IndexedDatasetJava {
         IndexedDatasetSpark newids =  IndexedDatasetSpark.apply(rdd.rdd(),
                                 OptionHelper.<BiDictionary>some(existingRowIDs.get().bdict), sc);
         return new IndexedDatasetJava(newids);
+    }
+
+    public IndexedDatasetJava newRowCardinality(int n){
+        return new IndexedDatasetJava(ids.matrix().newRowCardinality(n),
+                                      new BiDictionaryJava(ids.rowIDs()),
+                                      new BiDictionaryJava(ids.columnIDs()));
     }
 }
