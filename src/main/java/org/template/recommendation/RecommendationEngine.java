402e7bc2cc035ea89013b93749be3fb2b8dbd4f1
Chris Fifty <cjf92@cornell.edu>
2017-03-12 19:17:48 -0400

    local changes

--- src/main/java/org/template/recommendation/RecommendationEngine.java
+++ src/main/java/org/template/recommendation/RecommendationEngine.java
@@ -4,16 +4,18 @@ import org.apache.predictionio.controller.EmptyParams;
 import org.apache.predictionio.controller.Engine;
 import org.apache.predictionio.controller.EngineFactory;
 import org.apache.predictionio.core.BaseAlgorithm;
+import org.apache.predictionio.core.BaseDataSource;
 import org.apache.predictionio.core.BaseEngine;
+import scala.collection.immutable.Set;
 
 import java.util.Collections;
-import java.util.Set;
+//import java.util.Set;
 
 public class RecommendationEngine extends EngineFactory {
 
     @Override
     public BaseEngine<EmptyParams, Query, PredictedResult, Set<String>> apply() {
-        return new Engine(
+        return new Engine<>(
                 DataSource.class,
                 Preparator.class,
                 Collections.<String, Class<? extends BaseAlgorithm<PreparedData, ?, Query, PredictedResult>>>singletonMap("ur", Algorithm.class),
