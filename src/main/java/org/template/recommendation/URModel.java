3713a5d70b14435ffb9b133e8b4cd6b7ebe79dec
Richard Henwood <rbh288@cornell.edu>
2017-03-14 00:15:02 -0400

    Write groupAll function

--- src/main/java/org/template/recommendation/URModel.java
+++ src/main/java/org/template/recommendation/URModel.java
@@ -92,8 +92,12 @@ public class URModel {
 
     private JavaPairRDD<String, Map<String,JsonAST.JValue>> groupAll(
             List<JavaPairRDD<String, Map<String,JsonAST.JValue>>> fields) {
-        // fields.fold(sc.emptyRDD[(ItemID, ItemProps)])(_ ++ _).reduceByKey(_ ++ _)
-        return null;
+
+        JavaPairRDD<String, Map<String,JsonAST.JValue>> acc = RDDUtils.getEmptyPairRDD(sc);
+        for (JavaPairRDD<String, Map<String,JsonAST.JValue>> field : fields) {
+            acc.union(field);
+        }
+        return acc.reduceByKey((m1, m2) -> {m1.putAll(m2); return m1;});
     }
 
     private static Object extractJvalue(List<String> dateNames, String key, Object value) {
