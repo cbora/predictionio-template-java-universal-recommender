6e219af420aa954d7badafe0a53c041f6acbd330
Ji Hun Kim <jk2227@cornell.edu>
2017-04-07 23:52:34 -0400

    shell for query test

--- src/test/java/org/template/QueryTest.java
+++ src/test/java/org/template/QueryTest.java
@@ -9,21 +9,6 @@ public class QueryTest {
 
     @Test
     public void constructorTest() throws Exception {
-      String user = "user"; // must be a user or item id
-      Float userBias = 0.0f; // default: whatever is in algorithm params or 1
-      String item = "item"; // must be a user or item id
-      Float itemBias = 0.0f; // default: whatever is in algorithm params or 1
-      List<Field> fields = new LinkedList<>(); // default: whatever is in algorithm params or None
-      String currentDate = "20170327"; // if used will override dateRange filter, currentDate must lie between the item's
-      // expireDateName value and availableDateName value, all are ISO 8601 dates
-      DateRange dateRange; // optional before and after filter applied to a date field
-      List<String> blacklistItems = new LinkedList<>(); // default: whatever is in algorithm params or None
-      Boolean returnSelf; // means for an item query should the item itself be returned, defaults
-      // to what is in the algorithm params or false
-      Integer num; // default: whatever is in algorithm params, which itself has a default--probably 20
-      List<String> eventNames; // names used to ID all user actions
-      Boolean withRanks; // Add to ItemScore rank fields values, default false
-
 
     }
 
